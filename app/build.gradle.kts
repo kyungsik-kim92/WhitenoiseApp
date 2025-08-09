d336d3e157f8ea6ef30a43fcd9c2d9f71d3dce1f
KKS <ksiki1@naver.com>
2025-08-09 20:52:36 +0900

    compose 라이브러리 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,10 +38,31 @@ android {
     buildFeatures {
         viewBinding = true
         dataBinding = true
+        compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.14"
     }
 }
 
 dependencies {
+
+    implementation(platform("androidx.compose:compose-bom:2025.07.00"))
+
+
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.material3:material3")
+    implementation("androidx.activity:activity-compose:1.10.1")
+
+
+    implementation("androidx.navigation:navigation-compose:2.9.3")
+
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.9.2")
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.9.2")
+
+    implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
@@ -58,6 +79,4 @@ dependencies {
     implementation(libs.glide)
     annotationProcessor(libs.glide.compiler)
 
-    // admob
-//    implementation(libs.play.services.ads)
 }
