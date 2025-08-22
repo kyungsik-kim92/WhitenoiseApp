90c4423b2b030eaa1429133f86ebddd1ddc3d880
KKS <ksiki1@naver.com>
2025-08-22 22:26:47 +0900

    mockk 라이브러리 추가

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -37,19 +37,20 @@ android {
 dependencies {
     implementation(project(":domain"))
     implementation(project(":core"))
-    
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
-    
+
     // Hilt
     implementation("com.google.dagger:hilt-android:2.48.1")
     kapt("com.google.dagger:hilt-compiler:2.48.1")
-    
+
     // Coroutines
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
-    
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+
+    testImplementation("io.mockk:mockk:1.13.4")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.4")
+    testImplementation("org.jetbrains.kotlin:kotlin-test:1.8.10")
+    testImplementation("junit:junit:4.13.2")
 }
