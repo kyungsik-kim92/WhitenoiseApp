75820659572ba2889ac696a470f9945ae6a5a995
KKS <ksiki1@naver.com>
2025-09-06 20:41:28 +0900

    gradlePlugin추가

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -3,15 +3,18 @@ plugins {
     `kotlin-dsl-precompiled-script-plugins`
 }
 
-dependencies{
+dependencies {
     implementation(libs.android.gradlePlugin)
     implementation(libs.kotlin.gradlePlugin)
+
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.24")
+    implementation("com.android.tools.build:gradle:8.7.3")
 }
 
-gradlePlugin{
-    plugins{
-        register("androidHilt"){
-            id= "whitenoiseapp.hilt.android"
+gradlePlugin {
+    plugins {
+        register("androidHilt") {
+            id = "whitenoiseapp.hilt.android"
             implementationClass = "com.example.whitenoiseapp.HiltAndroidPlugin"
         }
     }
