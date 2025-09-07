28597edfb30ee420df4d82dc491791770c4a393d
KKS <ksiki1@naver.com>
2025-09-07 23:09:45 +0900

    whitenoiseapp.android.library.gradle.kts

---
+++ build-logic/src/main/kotlin/whitenoiseapp.android.library.gradle.kts
@@ -0,0 +1,10 @@
+import com.example.whitenoiseapp.configureHiltAndroid
+import com.example.whitenoiseapp.configureKotlinAndroid
+
+
+plugins{
+    id("com.android.library")
+}
+
+configureKotlinAndroid()
+configureHiltAndroid()
