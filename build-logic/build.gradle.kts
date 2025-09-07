cedf54769c20fea37352c733d80df0b8cf21f923
KKS <ksiki1@naver.com>
2025-09-07 23:13:43 +0900

    libs.kotlin.gradlePlugin 수정

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -6,9 +6,6 @@ plugins {
 dependencies {
     implementation(libs.android.gradlePlugin)
     implementation(libs.kotlin.gradlePlugin)
-
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.24")
-    implementation("com.android.tools.build:gradle:8.7.3")
 }
 
 gradlePlugin {
