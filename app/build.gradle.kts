5792a3d1c2a1d08b69663eca16ce6e42e47d1b9a
KKS <ksiki1@naver.com>
2025-09-15 22:10:15 +0900

    라이브러리 중복제거

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,9 +32,7 @@ dependencies {
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+
 
 
     implementation(libs.coil.compose)
