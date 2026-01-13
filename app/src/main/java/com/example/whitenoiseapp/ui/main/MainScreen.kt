b2bf39da5b7f3f2490781bb98bb3cc287c1ba40e
KKS <ksiki1@naver.com>
2026-01-13 15:55:04 +0900

    바텀네비게이션 색상 변경

--- app/src/main/java/com/example/whitenoiseapp/ui/main/MainScreen.kt
+++ app/src/main/java/com/example/whitenoiseapp/ui/main/MainScreen.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -105,12 +106,18 @@ fun MainBottomBar(navigator: MainNavigator) {
                     Icon(
                         painter = painterResource(id = item.iconRes),
                         contentDescription = item.title,
-                        modifier = Modifier.size(20.dp)
+                        modifier = Modifier.size(20.dp),
+                        tint = if (currentRoute == item.route)
+                            Color(0xFF4DB6AC)
+                        else
+                            Color(0xFF9E9E9E)
                     )
+
                 },
                 label = { Text(item.title) },
                 selected = currentRoute == item.route,
-                onClick = item.onClick
+                onClick = item.onClick,
+                colors = NavigationBarItemDefaults.colors(indicatorColor = Color.Transparent)
             )
         }
     }
